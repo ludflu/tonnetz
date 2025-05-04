8bfbb0e7bec59797cd31813a9bf1ddc14b2953f6
Jim Snavely <ludflu@gmail.com>
2025-05-04 14:33:30 -0400

    arp (#8)

--- lib/NotesToEuterpea.hs
+++ lib/NotesToEuterpea.hs
@@ -35,22 +35,31 @@ renderTriad dur' (NeoRiemann.Triad root third fifth _) =
          sound fifth dur' ]
     in chord'  :+: rest (1/4)
 
-
+renderArpTriad :: Dur -> NeoRiemann.Triad -> Music Pitch
+renderArpTriad dr (NeoRiemann.Triad root third fifth _) = 
+  -- Play the three notes as a chord (simultaneously)
+  let chord' = chord  [sound root dr, 
+         sound  third dr, 
+         sound fifth dr ]
+      mel = sound root (dr/4) :+: sound third (dr/4) :+: sound fifth (dr/4)
+    in ( mel :=: chord'):+:  rest (1/4)
+    
 renderTriadSequence :: Dur -> [NeoRiemann.Triad] -> Music Pitch
-renderTriadSequence dur' triads = 
-  -- Create a sequence of triads
-  let music = foldr1 (:+:) $ map (renderTriad dur') triads
-  in music
+renderTriadSequence dur' triads =  foldr1 (:+:) $ map (renderTriad dur') triads
+
+renderArpTriadSequence :: Dur -> [NeoRiemann.Triad] -> Music Pitch
+renderArpTriadSequence dr triads =  foldr1 (:+:) $ map (renderArpTriad dr) triads
+
 
 playTriads :: [NeoRiemann.Triad] -> Integer -> IO ()
 playTriads triads dur' = do
   let duration' = 1 / fromIntegral dur' :: Dur
-      music = renderTriadSequence duration' triads 
+      music = renderArpTriadSequence duration' triads 
    in play $ rest duration' :+: music :+: rest duration'
 
 
 writeTriads :: FilePath -> [NeoRiemann.Triad] -> Integer -> IO ()
 writeTriads fp triads dur' = do
   let duration' = 1 / fromIntegral dur' :: Dur
-      music = renderTriadSequence duration' triads 
+      music = renderArpTriadSequence duration' triads 
    in writeMidi fp $ rest duration' :+: music :+: rest duration'
